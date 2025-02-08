b5f3a46fd24f2c7360cb922323bdccf8a13d963f
mohit <mohit21339@iiitd.ac.in>
2025-02-09 01:52:39 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          lavender: '#ca6be5',
+          navy: '#0e2675',
+          emerald: '#008000'
+        }
+      },
+    },
+    darkMode: 'false',
+    plugins: [],
+  }
